6b56a5329b0d56d14c5a2e29bcc72edf58bc180a
daniel1181888 <94381025+daniel1181888@users.noreply.github.com>
2024-10-10 13:46:10 +0200

    Added inputs and button for GUI

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from gettext import textdomain
+from tkinter import Entry
 
 root = tk.Tk()
 root.title("Music Downloader")
@@ -9,5 +10,26 @@ root.geometry('1000x800')
 label = tk.Label(root,text="Music Downloader(by dj fdjesko)",font=("Helvetica",24,"bold"))
 label.pack()
 
+label = tk.Label(root,text="Spotify url", font=("Helvetica",15,"bold"))
+label.pack()
+
+downloadurl = Entry(root, width = 100)
+downloadurl.pack()
+
+label = tk.Label(root,text="Download path", font=("Helvetica",15,"bold"))
+label.pack()
+
+downloadpath = Entry(root, width = 100)
+downloadpath.pack()
+
+def clicked():
+    label.configure(text = "Downloading")
+
+btn = tk.Button(root, text = "download" ,
+             fg = "green", command=clicked)
+
+btn.pack()
+
 # Start the GUI event loop
 root.mainloop()
+
